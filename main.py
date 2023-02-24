fec03dbe20e69d5b38cb753c9c75da50ab2e2786
Kresh195 <KreshF5@mail.ru>
2023-02-24 18:47:36 +0700

    Revert "Merge branch 'main' of https://github.com/TriNitki/Friend-and-Partner-Finder"
    This reverts commit 1c87c999b154960f7ec403ca01950b00dd6fa3ba, reversing
    changes made to 65c398bfe3f6c3f99ab38c7e881b247a09221e05.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+#bot
+bot_token = os.getenv("bot_token")
+bot = telebot.TeleBot(bot_token)
 
 class User:
     def __init__(self, id, first_name = None, second_name = None, age = None, sex = None) -> None:
@@ -16,21 +19,12 @@ class User:
     def get_data(self):
         return self.id, self.first_name, self.second_name, self.age, self.sex
 
-#bot
-bot_token = os.getenv("bot_token")
-bot = telebot.TeleBot(bot_token)
-
-#sqlite3
-connect = sqlite3.connect('users.sqlite3', check_same_thread=False)
-cursor = connect.cursor()
-
 
 @bot.message_handler(content_types=['text'])
-def main(message):
-    global user
-    user = User(message.chat.id) # Define user
-
-    # Create new db if it isn't exist
+def start(message):
+    connect = sqlite3.connect('users.sqlite3', check_same_thread=False)
+    cursor = connect.cursor()
+    user = User(message.chat.id)
     cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
         id          INTEGER,
         first_name  STRING,
@@ -40,23 +34,56 @@ def main(message):
     )""")
     connect.commit()
 
-    cursor.execute(f"SELECT id FROM login_id WHERE id = {user.id}")
-    data = cursor.fetchone()
+    def greet_user():
+        cursor.execute(f"SELECT * FROM login_id")
+        users_data = list(cursor.fetchone())
+        user = User(users_data[0], users_data[1], users_data[2], users_data[3], users_data[4])
+        bot.send_message(message.chat.id, f'Привет, {user.second_name} {user.first_name}. Тебе {user.age} {year_type(user.age)}.')
+    
+    def fill_form(message):
+        bot.send_message(message.chat.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, first_name)
 
-    '''User commands'''
+    def first_name(message):
+        user.first_name = message.text
+        bot.send_message(message.chat.id, 'Какая у тебя фамилия?')
+        bot.register_next_step_handler(message, second_name) 
+
+    def second_name(message):
+        user.second_name = message.text
+        bot.send_message(message.chat.id, 'Сколько тебе лет?')
+        bot.register_next_step_handler(message, age)
+    
+    def age(message):
+        if user.age == None:
+            try:
+                user.age = int(message.text)
+            except:
+                bot.reply_to(message, 'Может лучше цифрами введешь?')
+                bot.register_next_step_handler(message, age)
+                return
+        cursor.execute("INSERT INTO login_id VALUES(?, ?, ?, ?, ?);", user.get_data())
+        connect.commit()
+        greet_user()
+
+
     if message.text == "/start":
-        # greet mew user
         bot.send_message(message.chat.id, f'Привет, {message.chat.username}👋!\
                          \nДобро пожаловать в "DetectyBot"! Этот бот создан для поиска друзей и бизнес партнеров.')
         bot.send_message(message.chat.id, 'Для начала регистрации введите команду /reg. Для полного списка возможностей введите /help.')
     elif message.text == "/reg":
-        #register user if it isn't exist
+        cursor.execute(f"SELECT id FROM login_id WHERE id = {user.id}")
+        data = cursor.fetchone()
+
         if data == None:
-            reg_user(message)
+            #add value in fields
+            fill_form(message)
         else:
             bot.send_message(message.chat.id, 'Данный пользователь уже зарегестрирован.')
     elif message.text == "/delete":
-        #delete user if it is exist
+        cursor.execute(f"SELECT id FROM login_id WHERE id = {user.id}")
+        data = cursor.fetchone()
+
         if data == None:
             bot.send_message(message.chat.id, 'Пользователь еще не создан.\nДля регистрации воспользуйтесь командой /reg.')
         else:
@@ -64,13 +91,14 @@ def main(message):
             connect.commit()
             bot.send_message(message.chat.id, 'Пользователь был удален.\nДля повторной регистрации воспользуйтесь командой /reg.')
     elif message.text == "/me":
-        #greet user if it is exist
+        cursor.execute(f"SELECT id FROM login_id WHERE id = {user.id}")
+        data = cursor.fetchone()
+
         if data == None:
             bot.send_message(message.chat.id, 'Пользователь еще не создан.\nДля регистрации воспользуйтесь командой /reg')
         else:
-            greet_user(message)
+            greet_user()
     elif message.text == "/help":
-        #send full list of commands
         bot.send_message(message.chat.id, 'Список комманд:')
         bot.send_message(message.chat.id, '/start - запуск бота\
                          \n/help - список комманд\
@@ -82,37 +110,6 @@ def main(message):
         bot.reply_to(message, '🤨')
         bot.send_message(message.chat.id, 'Не понял.\nДля начала регистрации введите команду /start.')
     
-def greet_user(message):
-    cursor.execute(f"SELECT * FROM login_id")
-    id, f_name, s_name, age, sex = cursor.fetchone()
-    user = User(id, f_name, s_name, age, sex)
-    bot.send_message(message.chat.id, f'Привет, {user.second_name} {user.first_name}. Тебе {user.age} {year_type(user.age)}.')
-    
-def reg_user(message):
-    bot.send_message(message.chat.id, "Как тебя зовут?")
-    bot.register_next_step_handler(message, first_name)
-
-def first_name(message):
-    user.first_name = message.text
-    bot.send_message(message.chat.id, 'Какая у тебя фамилия?')
-    bot.register_next_step_handler(message, second_name) 
-
-def second_name(message):
-    user.second_name = message.text
-    bot.send_message(message.chat.id, 'Сколько тебе лет?')
-    bot.register_next_step_handler(message, age)
-    
-def age(message):
-    if user.age == None:
-        try:
-            user.age = int(message.text)
-        except:
-            bot.reply_to(message, 'Может лучше цифрами введешь?')
-            bot.register_next_step_handler(message, age)
-            return
-    cursor.execute("INSERT INTO login_id VALUES(?, ?, ?, ?, ?);", user.get_data())
-    connect.commit()
-    greet_user(message)
 
 def year_type(age):
     if (age % 100 >= 11) and (age % 100 <= 14):
